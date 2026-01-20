04dfbab946533dddb8721e55fad29e50d993afe0
iDecisionTree <lhdzyc@163.com>
2026-01-20 11:33:34 +0800

    初始化仓库

---
+++ src/test/java/com/gjyyk/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjyyk.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
