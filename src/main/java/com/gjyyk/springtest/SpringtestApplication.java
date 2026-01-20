47d5bbf73465f107bf61cc723de8c0c355b0c02b
iDecisionTree <lhdzyc@163.com>
2026-01-20 21:56:57 +0800

    修复一些问题

--- src/main/java/com/gjyyk/springtest/SpringtestApplication.java
+++ src/main/java/com/gjyyk/springtest/SpringtestApplication.java
@@ -2,8 +2,9 @@ package com.gjyyk.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringtestApplication {
 
     public static void main(String[] args) {
